28029e30659fedf7ff91764710c7ec9a69f58d6b
Alejandro Alomia <alejandroalomia730@gmail.com>
2024-07-07 14:19:50 -0500

    refactoring and redeployment of infrastructure and film providers

---
+++ lib/presentation/providers/movies/movies_providers.dart
@@ -0,0 +1,25 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/providers/movies/movies_repository_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final nowPlayingMoviesProvider = StateNotifierProvider<MoviesNotifier, List<Movie>>((ref) {
+  final fetchMoreMovies = ref.watch(movieRepositoryProvider).getNowPlayingMovies;
+
+  return MoviesNotifier(fetchMorMovies: fetchMoreMovies);
+});
+
+typedef MovieCallback = Future<List<Movie>> Function({int page});
+
+class MoviesNotifier extends StateNotifier<List<Movie>> {
+  int currentPage = 0;
+  MovieCallback fetchMorMovies;
+
+  MoviesNotifier({required this.fetchMorMovies}) : super([]);
+
+  Future<void> loadNextPage() async {
+    currentPage++;
+    final List<Movie> movies = await fetchMorMovies(page: currentPage);
+
+    state = [...state,...movies];
+  }
+}
